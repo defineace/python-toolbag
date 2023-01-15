7e6f5ae16e684b88c078cf74016e4b9bb70d06e8
Leandro Rico <>
2023-01-14 16:47:45 -0800

    Description:  - Added more functionality to explorer

--- component_explorer.py
+++ component_explorer.py
@@ -39,12 +39,56 @@ class tab_explorer:
         search_button.grid(row=0, column=2, rowspan=2)
 
 
+    def createTableHeader(self):
+        # Variables
+        prefix = StringVar()
+
+
+        # Widget Root
+        frame_tableHeader = Frame(self.root)
+        self.frameList.append(frame_tableHeader)
+        frame_tableHeader.pack()
+
+        # Widgets
+        filename_prefix_label = Label(frame_tableHeader, text="File Name Prefix")
+        filename_prefix_entry = Entry(frame_tableHeader)
+
+        filename_root_label = Label(frame_tableHeader, text="File Name Root")
+        filename_root_entry = Entry(frame_tableHeader)
+
+        filename_suffix_label = Label(frame_tableHeader, text="File Name Suffix")
+        filename_suffix_entry = Entry(frame_tableHeader)
+
+        filename_delimiter_label = Label(frame_tableHeader, text="File Name Delimiter")
+        filename_delimiter_entry = Entry(frame_tableHeader)
+
+        filename_index_label = Label(frame_tableHeader, text="File Name Indexing")
+        filename_index_entry = Entry(frame_tableHeader)
+
+        # Render
+        filename_prefix_label.grid(row=0, column=0)
+        filename_prefix_entry.grid(row=1, column=0)
+
+        filename_root_label.grid(row=0, column=1)
+        filename_root_entry.grid(row=1, column=1)
+
+        filename_suffix_label.grid(row=0, column=2)
+        filename_suffix_entry.grid(row=1, column=2)
+
+        filename_delimiter_label.grid(row=0, column=3)
+        filename_delimiter_entry.grid(row=1, column=3)
+
+        filename_index_label.grid(row=0, column=4)
+        filename_index_entry.grid(row=1, column=4)
+
     def createTable(self):
         # Clear Table
         if(len(self.frameList) != 0):
             for frame in self.frameList:
                 frame.destroy()
-        
+
+        self.createTableHeader()
+
         # Widget Root
         frame_table = Frame(self.root)
         self.frameList.append(frame_table)
@@ -61,6 +105,19 @@ class tab_explorer:
 
                 filename_original = StringVar()
                 filename_original.set(filename)
+
+                """
+                if(filename.__contains__('.')):
+                    index_label.set("File: " + str(index_file))
+                    index_file += 1
+                else:
+                    index_label.set("Directory: " + str(index_dir))
+                    index_dir += 1
+
+                filename_original = StringVar()
+                filename_original.set(filename)
+                """
+
 
                 # Widgets
                 index_entry = Entry(frame_table, textvariable=index_label)
